0ff678b78347f924fed3a944a91522fc3e44b568
idasevia <idasevia09@gmail.com>
2023-02-17 12:43:40 +0700

    first commit

---
+++ Minggu2/app/src/main/java/com/example/minggu2/ConstraintLayout.java
@@ -0,0 +1,13 @@
+package com.example.minggu2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
