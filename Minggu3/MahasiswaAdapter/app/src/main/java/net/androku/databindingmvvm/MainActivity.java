8bd858ee59e323cb0438b7cf1f2dac1ec21ef6e7
idasevia <idasevia09@gmail.com>
2023-03-01 13:57:57 +0700

    Minggu 3

--- Minggu3/MahasiswaAdapter/app/src/main/java/net/androku/databindingmvvm/MainActivity.java
+++ Minggu3/MahasiswaAdapter/app/src/main/java/net/androku/databindingmvvm/MainActivity.java
@@ -37,5 +37,11 @@ MainActivity extends AppCompatActivity {
         mahasiswaArrayList.add(new Mahasiswa("Duwi Wirda", "1214234560", "987654321"));
         mahasiswaArrayList.add(new Mahasiswa("Ida Sevia", "1214230345", "987648765"));
         mahasiswaArrayList.add(new Mahasiswa("Citra Fionita", "1214378098", "098758124"));
+        mahasiswaArrayList.add(new Mahasiswa("Fransisca Ani", "1210912035", "980028765"));
+        mahasiswaArrayList.add(new Mahasiswa("Della Monica", "1216573889", "098988765"));
+        mahasiswaArrayList.add(new Mahasiswa("Leo Adi", "1214230212", "987611165"));
+        mahasiswaArrayList.add(new Mahasiswa("Aprilia Adila", "1214378445", "096508124"));
+        mahasiswaArrayList.add(new Mahasiswa("Adinda", "1894230345", "087648765"));
+        mahasiswaArrayList.add(new Mahasiswa("Aristo Kai", "1109378098", "088758124"));
     }
 }
